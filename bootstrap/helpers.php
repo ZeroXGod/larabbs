1f72782e0eb341539d2a4912c6f598f8835c6598
Zero <s812419396@gmail.com>
2018-09-12 06:47:33 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,8 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function make_excerpt($value, $length = 200)
+{
+    $except = trim(preg_replace('/\r\n|\r|\n+/', '', strip_tags($value)));
+}
