7acf2c087128fad2a1f6d3e3bfcd453a0498a743
ZeroXGod <s812419396@gmail.com>
2018-12-07 01:44:32 +0000

    weapp users store

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -40,6 +40,45 @@ class UsersController extends Controller
             ->setStatusCode(201);
     }
 
+    public function weappStore(UserRequest $request)
+    {
+        // 缓存中是否存在对应的 key
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        // 判断验证码是否相等，不相等返回 401 错误
+        if (!hash_equals((string)$verifyData['code'], $request->verification_code)) {
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        // 获取微信的 openid 和 session_key
+        $miniProgram = \EasyWeChat::miniProgram();
+        $data = $miniProgram->auth->session($request->code);
+
+        if (isset($data['errcode'])) {
+            return $this->response->errorUnauthorized('code 不正确');
+        }
+
+        // 如果openid 对应的用户
+        $user = User::where('weapp_openid', $data['openid'])->first();
+
+        if ($user) {
+            return $this->response->errorForbidden('微信已绑定其他用户, 请直接登录');
+        }
+
+        // 创建用户
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+            'weapp_openid' => $data['openid'],
+            'weixin_session_key' => $data['session_key'],
+        ]);
+    }
+
     public function update(UserRequest $request)
     {
         $user = $this->user();
